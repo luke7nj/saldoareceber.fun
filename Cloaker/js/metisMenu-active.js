0a8d63d290cfc2ec557bcf8e62a2d832307f87a0
Kawxs <102254268+Kawxs@users.noreply.github.com>
2023-10-14 16:44:45 -0300

    .

--- Cloaker/js/metisMenu-active.js
+++
@@ -1,1 +0,0 @@
-!function(e){"use strict";jQuery("#menu1").metisMenu()}();
--- scale/admin/version.php
+++
@@ -1,8 +0,0 @@
-<h2>
-    <a href="https://yellowweb.top/donate" target="_blank">
-        Version: 19.10.2022
-        <br/>
-        PHP Version: <?=phpversion()?>
-        <br/> Please, donate!
-    </a>
-</h2>
